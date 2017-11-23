8b7194e71f68961ec4a6a9f4020df5eb74b2f2b8
Samuel Bailey <samuel.bailey@cambridgeconsultants.com>
2017-11-23 11:58:27 +0000

    Changed the FindValidMoves function to work without the lists of white and black counters. Currently won't return the correct square though - needs fixing

--- Othello/Board.cs
+++ Othello/Board.cs
@@ -17,8 +17,8 @@ namespace Othello
     {
         public int size;
         public Colour[,] board;
-        List<Position> blackPositions;
-        List<Position> whitePositions;
+        //List<Position> blackPositions;
+        //List<Position> whitePositions;
         public List<Move> possibleMoves;
         
         /// <summary>
@@ -27,8 +27,8 @@ namespace Othello
         /// <param name="size">Must be even and >= 4</param>
         public Board(int size)
         {
-            blackPositions = new List<Position>();
-            whitePositions = new List<Position>();
+            //blackPositions = new List<Position>();
+            //whitePositions = new List<Position>();
             if (size % 2 == 1 || size < 4)
             {
                 throw new Exception("size must be an even integer >= 4");
@@ -50,14 +50,114 @@ namespace Othello
                     if ((i + j) % 2 == 0)
                     {
                         board[j, i] = Colour.white;
-                        whitePositions.Add(new Position(j, i));
+                        //whitePositions.Add(new Position(j, i));
                     }
                     else
                     {
                         board[j, i] = Colour.black;
-                        blackPositions.Add(new Position(j, i));
+                        //blackPositions.Add(new Position(j, i));
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of Positions which the player can move to 
+        /// </summary>
+        /// <param name="playerColour"></param>
+        /// <returns></returns>
+        public List<Position> FindValidMoves(Colour playerColour)
+        {
+            if (playerColour == Colour.none)
+            {
+                throw new ArgumentException("colour must not be none");
+            }
+
+            List<Position> validMoves = new List<Position>();
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    // TODO: update this algorithm. Currently returns the positions of the player's counter already on the board, rather than the
+                    // position of the empty square that the player needs to place a counter in.
+                    Position currentPosition = new Position(j, i);
+                    if (CheckSurroundingSquares(currentPosition, playerColour, Opposite(playerColour)))
+                    {
+                        validMoves.Add(currentPosition);
+                    }
+                }
+            }
+            return validMoves;
+        }
+
+        // TODO: This should return the empty square at the end of the recursive call instead of a boolean value;
+        bool CheckSurroundingSquares(Position counterPosition, Colour playerColour, Colour enemyColour)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (CheckMove(counterPosition, new Position(j, i), playerColour, enemyColour))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks that there is at least one enemy counter before a counter of the player's colour before calling FindLineRecursively().
+        /// </summary>
+        /// <param name="counterPosition">Position of the player's counter to find whether or not there are valid moves from this counter</param>
+        /// <param name="direction">Position with maximum size of 1 which sets the direction to convert</param>
+        /// <param name="playerColour"></param>
+        /// <returns>True if there is a line that the player can convert, otherwise false.</returns>
+        bool CheckMove(Position counterPosition, Position direction, Colour playerColour, Colour enemyColour)
+        {
+            Position nextPosition = counterPosition.Add(direction);
+            if (!OnBoard(nextPosition))
+            {
+                return false;
+            }
+            else if (board[nextPosition.x, nextPosition.y] == enemyColour)
+            {
+                return FindLineRecursively(nextPosition, direction, playerColour, enemyColour);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Recursively calls itself while the next counter is of the enemy's colour. Will return true if it reaches a 
+        /// counter of the player's colour and will retun false if it reaches an empty square or the edge of the board.
+        /// </summary>
+        /// <param name="currentPosition"></param>
+        /// <param name="direction"></param>
+        /// <param name="playerColour"></param>
+        /// <param name="enemyColour"></param>
+        /// <returns></returns>
+        bool FindLineRecursively(Position currentPosition, Position direction, Colour playerColour, Colour enemyColour)
+        {
+            Position nextPosition = currentPosition.Add(direction);
+            if (!OnBoard(nextPosition))
+            {
+                return false;
+            }
+            else if (board[nextPosition.x, nextPosition.y] == enemyColour)
+            {
+                return FindLineRecursively(nextPosition, direction, playerColour, enemyColour);
+            }
+            else if (board[nextPosition.x, nextPosition.y] == Colour.none)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
@@ -66,20 +166,24 @@ namespace Othello
         /// </summary>
         /// <param name="colour">The Colour of the player taking their turn. Must not be Colour.none</param>
         /// <returns></returns>
-        public List<Position> FindValidMoves(Colour colour)
+        /*public List<Position> FindValidMoves(Colour colour)
         {
-            List<Position> enemyPieces;
-
-            switch (colour)
+            if (colour == Colour.none)
             {
-                case Colour.white:
-                    enemyPieces = blackPositions;
-                    break;
-                case Colour.black:
-                    enemyPieces = whitePositions;
-                    break;
-                default:
-                    throw new ArgumentException("Colour must be Colour.black or Colour.white");
+                throw new ArgumentException("colour must not be none");
+            }
+            List<Position> enemyPieces = new List<Position>();
+            Colour enemyColour = Opposite(colour);
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[j, i] == enemyColour)
+                    {
+                        enemyPieces.Add(new Position(j, i));
+                    }
+                }
             }
 
             List<Position> possibleMoves = new List<Position>();
@@ -112,7 +216,7 @@ namespace Othello
             }
 
             return possibleMoves;
-        }
+        }*/
 
         /// <summary>
         /// Recursively calls to find whether or not a line of counters is present to be flipped
--- Othello/Position.cs
+++ Othello/Position.cs
@@ -20,11 +20,17 @@ namespace Othello
         /// Adds the x and y coordinates of the position parameter to this Position.
         /// </summary>
         /// <param name="position"></param>
+        /// <returns>Position containing the x and y values calculated from the sum of the positions</returns>
         public Position Add(Position position)
         {
             return new Position(this.x + position.x, this.y + position.y);
         }
 
+        /// <summary>
+        /// Adds the x and y coordinates of the position parameter to this object's x and y variables.
+        /// The result is stored in this object's x and y variables.
+        /// </summary>
+        /// <param name="position"></param>
         public void AddToSelf(Position position)
         {
             this.x += position.x;
